eb2b9515a59ea81b20b1b0e21e51f044204320ee
B1129017 <113400100+xshanyun@users.noreply.github.com>
2023-01-04 04:17:12 +0800

    Update main5.c

--- HW8/main5.c
+++ HW8/main5.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
-void body(int rows,int shift){
-    int i, space, k = 0;
-    
-    for (i=1; i<= rows; ++i, k=0) {
-        for (int i=2; i<=shift; i++)
-            printf("  ");
-        for (space = 1; space <= rows - i; ++space)
-            printf("  ");
-        for(int k=0; k!=2*i-1; k++)
-            printf("* ");
-        printf("\n");
+
+void print_spaces(int i, int rows){
+    for(int k=rows, mid=7;k<mid;k++){
+        printf(" ");
+    }
+    for(int spc=1;spc<=rows-i;spc++){
+        printf(" ");
+    } 
+}
+void print_stars(int i){
+    for(int str=1;str<=(i+1)*2-1;str++){
+        printf("*");
     }
 }
-int main() {
-    int top = 3;
-    int truck = top;
-    printf("X'mas tree\n");
-    for(int i=top;i>0;i--)
-        body(top++, i);
-    for(int i=truck;i>0;i--)
-        body(1, truck*2-1);
+void print_top(int rows){
+    for(int i=0;i<rows;i++){
+        
+        print_spaces(i,rows);
+        print_stars(i);
+        printf("\n");
+       
+    }
+}
+void print_truck(int T){
+    for(int s=0;s<T;s++)
+    printf("       *\n");
+}
+   
+int main()
+{
+    int T=5;
+    printf("   X'mas tree\n");
+    for(int rows=3;rows<=7;rows=rows+2){
+      print_top(rows);
+      
+    }
+    print_truck(T);
+
     return 0;
 }
